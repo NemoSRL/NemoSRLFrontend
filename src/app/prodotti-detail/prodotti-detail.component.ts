87af600c33a0f1921293ab3778f89fdd8dfbe962
DividingByZeros <davide.pupino1@gmail.com>
2024-05-14 02:57:06 +0200

    some kind of navbar

--- src/app/prodotti-detail/prodotti-detail.component.ts
+++ src/app/prodotti-detail/prodotti-detail.component.ts
@@ -4,12 +4,19 @@ import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 import {NgIf, UpperCasePipe} from '@angular/common';
 import {FormsModule} from '@angular/forms';
-
+import { COUNTRIES } from '../mock-countries';
 @Component({
   selector: 'app-prodotti-detail',
   templateUrl: './prodotti-detail.component.html',
   styleUrl: './prodotti-detail.component.css'
 })
+
+
 export class ProdottiDetailComponent {
-  @Input()country?: Country;
+  constructor(
+    private route: ActivatedRoute,
+    private location: Location
+  ) {}
+  country = COUNTRIES[Number(this.route.snapshot.paramMap.get('id'))-1];
+  
 }
