175652381b75c3ca7ce4cf4648a17be0ea67731e
DividingByZeros <davide.pupino1@gmail.com>
2024-05-16 10:04:00 +0200

    navbar works now! :)

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+  isSidebarOpen = false;
+  activeSubMenu: string | null = null;
 
+  toggleSidebar() {
+    this.isSidebarOpen = !this.isSidebarOpen;
+  }
+
+  showSubMenu(menuItem: string) {
+    this.activeSubMenu = menuItem;
+  }
+
+  hideSubMenu(menuItem: string) {
+    if (this.activeSubMenu === menuItem) {
+      this.activeSubMenu = null;
+    }
+  }
+
+  keepSubMenuOpen(menuItem: string) {
+    this.activeSubMenu = menuItem;
+  }
 }
